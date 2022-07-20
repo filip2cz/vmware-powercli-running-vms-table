3740baef9385ecb7855cbb210efc383c57e1dbf2
filip2cz <filip@fkomarek.eu>
2022-07-20 09:50:09 +0200

    only test if everything is working after renamed repository

--- runnningvms.ps1
+++ runnningvms.ps1
@@ -1,4 +1,4 @@
-# You need to connect to server first with this command:
+# You need to connect to server first with this command:d
 # connect-viserver [adress] -User [user] -Password [password] -Force
 # something like connect-viserver 192.168.1.50 -User administrator@vsphere.local -Password 12345 -Force
 
